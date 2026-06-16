6232df4628d67764791c8036c6a02d83ff19e165
Marco Aurelio Hansen de Oliveira <marco.hansen@venturus.org.br>
2026-06-16 11:22:43 -0300

    ajuste para funcionar o drawer

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import { NavigationContainer, StackRouter } from '@react-navigation/native';
 import { TabRoutes } from './src/routes/TabRoutes';
 import { DrawerRoutes } from './src/routes/DrawerRoutes';
 import { StackRoutes } from './src/routes/StackRoutes';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* <TabRoutes /> */}
-      {/* <DrawerRoutes /> */}
-      <StackRoutes />
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        {/* <TabRoutes /> */}
+        <DrawerRoutes />
+        {/* <StackRoutes /> */}
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
